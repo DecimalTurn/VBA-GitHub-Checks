bff54d523a526860a105202d51d7d19e6c1e79df
Martin Leduc <31558169+DecimalTurn@users.noreply.github.com>
2024-08-19 14:49:15 -0400

    Create check_for_changes.py

---
+++ .github/workflows/check_for_changes.py
@@ -0,0 +1,11 @@
+#Q: Should the outter loop be relative to the different Checks (A, B, C , etc) or one issue at a time?
+#A: from a degugging perspective, it would be nicer to have the logs grouped by types of checks to see how things behave
+
+#todo:
+# - Collect all open issues
+# - Filter by check/issue
+# - CheckA:
+# - Extract user and repo_name (repo slug)
+# - Get the infos on the repo
+# - Check the infos to see if language is not VBA
+# - If yes, close the issue and write a comment.
