78cabca1e883913759f91add249ad17765701903
Martin Leduc <31558169+DecimalTurn@users.noreply.github.com>
2024-08-25 14:19:29 +0000

    Add notes

--- .github/workflows/check_for_changes.py
+++ .github/workflows/check_for_changes.py
@@ -1,5 +1,6 @@
 #Q: Should the outter loop be relative to the different Checks (A, B, C , etc) or one issue at a time?
 #A: from a degugging perspective, it would be nicer to have the logs grouped by types of checks to see how things behave
+#   from a preformance standpoint, it might be slightly better to have the outter loop be the Issues-iterator, but that should be minimal
 
 #todo:
 # - Collect all open issues
@@ -9,3 +10,6 @@
 # - Get the infos on the repo
 # - Check the infos to see if language is not VBA
 # - If yes, close the issue and write a comment.
+# - If no, check if at least some files now have the extension .vba
+    # - If yes, write a message saying that it's great that they changing the name of files from .vb to .vba, but there are still files with the .vb extension. Is that volontrary.
+
