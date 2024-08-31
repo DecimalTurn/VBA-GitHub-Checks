8289f15a53523aa8df3fe4e74d59ddcad3e541bb
Martin Leduc <31558169+DecimalTurn@users.noreply.github.com>
2024-08-31 16:52:31 +0000

    Moving elements to gh util

---
+++ .github/workflows/utils/gh.py
@@ -0,0 +1,51 @@
+# Github API utilities
+import requests
+
+def get_all_issues(token, repo_slug):
+    global all_issues
+    url = f"https://api.github.com/repos/{repo_slug}/issues"
+    headers = {
+        'Accept': 'application/vnd.github.v3+json',
+        'Authorization': f'token {token}'
+    }
+    
+    params = {
+        'per_page': 100,  # Maximum number of issues per page
+        'state': 'all',   # Fetch all issues, open and closed
+        'page': 1         # Start with the first page
+    }
+    
+    all_issues = []
+    
+    while True:
+        response = requests.get(url, headers=headers, params=params)
+        
+        if response.status_code == 200:
+            issues = response.json()
+            all_issues.extend([issue['title'] for issue in issues])
+            
+            if len(issues) < 100:
+                # If fewer than 100 issues were returned, this is the last page
+                break
+            else:
+                # Move to the next page
+                params['page'] += 1
+        else:
+            print(f"🔴 Failed to fetch issues. Status code: {response.status_code}")
+            raise ValueError("Problem while fetching issues.")
+    
+    return all_issues
+
+def get_issue(token, repo_slug , issue_number):
+    url = f"https://api.github.com/repos/{repo_slug}/issues/{issue_number}"
+    headers = {
+        'Accept': 'application/vnd.github.v3+json',
+        'Authorization': f'token {token}'
+    }
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to fetch issue {issue_number}. Status code: {response.status_code}")
